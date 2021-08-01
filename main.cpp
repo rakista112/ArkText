79eee3c4fd23a1af0127634aafae26a82033cd97
Ray Alovera <rakista112@gmail.com>
2021-08-01 16:40:39 +0800

    - don't track imgui.ini - layout UI

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(640, 480), "");
+    sf::RenderWindow window(sf::VideoMode(1024, 768), "");
     window.setVerticalSyncEnabled(true);
     ImGui::SFML::Init(window);
 
@@ -17,7 +17,7 @@ int main()
 
     // let's use char array as buffer, see next part
     // for instructions on using std::string with ImGui
-    char windowTitle[255] = "ImGui + SFML = <3";
+    char windowTitle[4096] = "ArkText";
 
     window.setTitle(windowTitle);
     window.resetGLStates(); // call it if you only draw ImGui. Otherwise not needed.
@@ -34,9 +34,10 @@ int main()
 
         ImGui::SFML::Update(window, deltaClock.restart());
 
-        ImGui::Begin("Sample window"); // begin window
+        ImGui::Begin("Sketch Editor"); // begin window
 
-                                       // Background color edit
+    /*
+        // Background color edit
         if (ImGui::ColorEdit3("Background color", color)) {
             // this code gets called if color value changes, so
             // the background color is upgraded automatically!
@@ -44,11 +45,11 @@ int main()
             bgColor.g = static_cast<sf::Uint8>(color[1] * 255.f);
             bgColor.b = static_cast<sf::Uint8>(color[2] * 255.f);
         }
-
+*/
         // Window title text edit
-        ImGui::InputText("Window title", windowTitle, 255);
+        ImGui::InputTextMultiline("", windowTitle, 4096, ImVec2(640, 480));
 
-        if (ImGui::Button("Update window title")) {
+        if (ImGui::Button("Run Sketch")) {
             // this code gets if user clicks on the button
             // yes, you could have written if(ImGui::InputText(...))
             // but I do this to show how buttons work :)
